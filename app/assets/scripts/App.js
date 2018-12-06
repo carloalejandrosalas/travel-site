edf86bebcb82675efb68b14e0d6c02fc14159335
Carlo Alejandro Salas <carlosalas2@gmail.com>
2018-12-05 23:09:48 -0700

    Completed the modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,10 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import $ from "jquery";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
 const mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($('.testimonial'), "60%");
-new StickyHeader();
+var stickyheader = new StickyHeader();
+var modal = new Modal();
