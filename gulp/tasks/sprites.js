313319797c3349b32c43362a038b3ac890d80a39
Carlo Alejandro Salas <carlosalas2@gmail.com>
2018-12-08 20:39:02 -0700

    Added browser support fallbacks

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp'),
 rename = require('gulp-rename'),
 svgSprite = require('gulp-svg-sprite'),
-del = require('del');
+del = require('del'),
+svg2png = require('gulp-svg2png');
 
 const config = {
+    shape: {
+        spacing: {
+            padding: 1
+        }
+    },
     mode : {
         css : {
+            variables: {
+                replaceSvgWithPng: function () {
+                    return function(sprite, render) {
+                        return render(sprite).split('.svg').join('.png');
+                    }
+                }
+            },
             sprite: 'sprite.svg',
             render: {
                 css : {
@@ -26,8 +39,14 @@ gulp.task('createSprite', ['beginClean'], () =>
     .pipe(gulp.dest('./app/temp/sprite/'))
 );
 
-gulp.task('copySpriteGraphic', ['createSprite'] , () => {
-    gulp.src('./app/temp/sprite/css/**/*.svg')
+gulp.task('createPngCopy',['createSprite'], () =>  
+    gulp.src('./app/temp/sprite/css/*.svg')
+    .pipe(svg2png())
+    .pipe(gulp.dest('./app/temp/sprite/css'))
+);
+
+gulp.task('copySpriteGraphic', ['createPngCopy'] , () => {
+    gulp.src('./app/temp/sprite/css/**/*.{svg,png}')
     .pipe(gulp.dest('./app/assets/images/sprites'))
 })
 
@@ -41,4 +60,4 @@ gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'] , () =>
     del(['./app/temp/sprite'])
 );
 
-gulp.task('icons', ['beginClean','createSprite', 'copySpriteGraphic' ,'copySpriteCSS', 'endClean']);
+gulp.task('icons', ['beginClean','createSprite', 'createPngCopy' ,'copySpriteGraphic' ,'copySpriteCSS', 'endClean']);
